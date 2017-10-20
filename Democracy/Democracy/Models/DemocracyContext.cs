34ee627ee71b535fd59cf0f41ce452b3cc511843
“MiguelFor” <miguelfor@hotmail.com>
2017-10-19 22:07:25 -0500

    Add module State

---
+++ Democracy/Democracy/Models/DemocracyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Models
+{
+    public class DemocracyContext : DbContext
+    {
+        public DemocracyContext()
+            : base("defaultConnection")
+        {
+        }
+
+        public DbSet<State> State { get; set; }
+    }
+}
---
+++ Democracy/Democracy/Models/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Models
+{
+    public class State
+    {
+        [Key]
+        public int StateI { get; set; }
+
+        public string Description { get; set; }
+    }
+}
