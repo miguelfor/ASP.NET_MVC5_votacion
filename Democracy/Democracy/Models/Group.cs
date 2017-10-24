df50ad2a4579f77ffc45734d60ba8963686792db
“MiguelFor” <miguelfor@hotmail.com>
2017-10-23 22:49:27 -0500

    Add States, Groups controller and  voting model

---
+++ Democracy/Democracy/Models/Group.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Models
+{
+    public class Group
+    {
+        /// <summary>
+        /// Gets or sets the grpup id
+        /// </summary>
+        [Key]
+        public int GroupI { get; set; }
+        /// <summary>
+        /// Gets or sets the group description
+        /// </summary>
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(50, ErrorMessage =
+                    "The field {0} can contain maximum {1} and minimum {2} character",
+                    MinimumLength = 3)]
+        public string Description { get; set; }
+    }
+}
---
+++ Democracy/Democracy/Models/Voting.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Models
+{
+    public class Voting
+    {
+        [Key]
+        public int VotingId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(50, ErrorMessage =
+                    "The field {0} can contain maximum {1} and minimum {2} character",
+                    MinimumLength = 3)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Display(Name ="State")]
+        public int StateId { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Remarks { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Display(Name = "Date Time Start")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime DateTimeStart { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime DateTimeEnd { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Display(Name = "Is For All Users")]
+        public bool IsForAllUsers { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Display(Name = "Is Enabled Blank Vote")]
+        public bool IsEnabledBlankVote { get; set; }
+
+        [Display(Name = "Quantity Votes")]
+        public int QuantityVotes { get; set; }
+        public int QuantityBlankVotes { get; set; }
+
+        [Display(Name = "Winner")]
+        public int CandidateWinID { get; set; }
+    }
+}
